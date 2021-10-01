760c0db12e06035f6b22dc8277b1e3e4133f01a4
Andres <andrezabala138@gmail.com>
2021-10-01 18:02:10 -0500

    Sincroniza Project

---
+++ EdDSAJwtBearer/EdDSAJwtBearerErrors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdDSAJwtBearer
+{
+    public static class EdDSAJwtBearerErrors
+    {
+        public const string ValidIssuerRequiered = "ValidIssuer is required when ValidateIssuer is true";
+    }
+}
---
+++ EdDSAJwtBearer/EdDSAJwtBearerServer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace EdDSAJwtBearer
+{
+    public class EdDSAJwtBearerServer
+    {
+        public EdDSAJwtBearerServerOptions EdDSAJwtBearerServerOptions { get; set; }
+
+        public EdDSAJwtBearerServer() { }
+
+        public EdDSAJwtBearerServer(EdDSAJwtBearerServerOptions options)
+        {
+            EdDSAJwtBearerServerOptions = options;
+        }
+
+        public string CreateToken(IEnumerable<Claim> claims,
+            string[] roles, DateTime expires) => EdDSATokenHandler.CreateToken(EdDSAJwtBearerServerOptions.PrivateSigningKey, EdDSAJwtBearerServerOptions.Issuer, 
+                EdDSAJwtBearerServerOptions.Audience, claims, roles, expires);
+        
+    }
+}
